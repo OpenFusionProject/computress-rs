7f00023cc1380ef176f4c63d890342404ed84a18
Gent Semaj <me@ycc.dev>
2025-01-02 00:34:29 -0500

    Have dedicated init outside of user data setup

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,32 @@
 use std::{process::exit, sync::Arc};
 
 use dotenv::dotenv;
-use poise::serenity_prelude::{ClientBuilder, GatewayIntents, Http};
+use poise::serenity_prelude::{ClientBuilder, GatewayIntents, Http, User};
 use tokio::sync::OnceCell;
 
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Result<T> = std::result::Result<T, Error>;
-type Context<'a> = poise::Context<'a, (), Error>;
 
 #[derive(Debug)]
 struct Globals {
+    bot_user: User,
     http: Arc<Http>,
 }
 
 static GLOBALS: OnceCell<Globals> = OnceCell::const_new();
+
+async fn on_init() -> Result<()> {
+    let globals = GLOBALS.get().unwrap();
+
+    let bot_user = &globals.bot_user;
+    println!(
+        "Logged in as {}#{} ({})",
+        bot_user.display_name(),
+        bot_user.discriminator.map_or(0, |d| d.get()),
+        bot_user.id
+    );
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() {
@@ -40,20 +53,31 @@ async fn main() {
         })
         .setup(|ctx, _ready, framework| {
             Box::pin(async move {
-                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-                let bot_user = ctx.http.get_current_user().await?;
-                println!(
-                    "Logged in as {}#{} ({})",
-                    bot_user.display_name(),
-                    bot_user.discriminator.map_or(0, |d| d.get()),
-                    bot_user.id
-                );
+                if let Err(e) =
+                    poise::builtins::register_globally(ctx, &framework.options().commands).await
+                {
+                    println!("Error while registering commands: {:?}", e);
+                };
+
+                let bot_user: User = match ctx.http.get_current_user().await {
+                    Ok(user) => user.into(),
+                    Err(e) => {
+                        println!("Error while getting current user: {:?}", e);
+                        exit(1);
+                    }
+                };
 
                 GLOBALS
                     .set(Globals {
+                        bot_user,
                         http: ctx.http.clone(),
                     })
                     .unwrap();
+
+                if let Err(e) = on_init().await {
+                    println!("Error while initializing: {:?}", e);
+                    exit(1);
+                }
                 Ok(())
             })
         })
