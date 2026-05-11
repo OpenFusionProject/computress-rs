0ca9c6ae2491bc12080026320ae718ea24691d4b
CakeLancelot <CakeLancelot@users.noreply.github.com>
2026-05-11 14:31:58 -0500

    Name search link functionality
    Adds a new config option that provides a quick link for moderators to search the requested name online.
    If not specified in the config, the button will not be present.

--- src/util.rs
+++ src/util.rs
@@ -1,17 +1,19 @@
 use poise::serenity_prelude::{ButtonStyle, ChannelId, CreateButton};
+use urlencoding::encode;
 
 use crate::{send_message_with_buttons, NameRequest, Result};
 
 pub(crate) async fn send_name_request_message(
     channel: ChannelId,
     name_request: &NameRequest,
+    search_template: &str,
 ) -> Result<()> {
     let messsage = format!(
         "Name request from Player {}: **{}**",
         name_request.player_uid, name_request.requested_name
     );
 
-    let buttons = vec![
+    let mut buttons = vec![
         CreateButton::new("namereq_approve")
             .label("Approve")
             .style(ButtonStyle::Success),
@@ -19,6 +21,12 @@ pub(crate) async fn send_name_request_message(
             .label("Deny")
             .style(ButtonStyle::Danger),
     ];
+
+    if !search_template.is_empty() {
+        let encoded_name = encode(&name_request.requested_name).into_owned();
+        let search_url = search_template.replace("{}", &encoded_name);
+        buttons.push(CreateButton::new_link(&search_url).label("Search"))
+    }
 
     send_message_with_buttons(channel, &messsage, buttons).await?;
     Ok(())
